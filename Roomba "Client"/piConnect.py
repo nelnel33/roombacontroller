0e61e9a168656828bd3d5e23f542e326218a3d98
Rishab Chhabra <Rishab.chhabra@stonybrook.edu>
2016-04-09 14:42:47 -0400

    Created python script to interface between local machine and roomba through ssh

---
+++ Roomba "Client"/piConnect.py
@@ -0,0 +1,13 @@
+import paramiko
+import time
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+info = open("info.txt",'r')
+ssh.connect(info.readline().strip(),username=info.readline().strip(),password=info.readline().strip())
+ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("python textController.py")
+time.sleep(5)
+ssh_stdin.write("forward\n")
+ssh_stdin.write("back\n")
+ssh_stdin.write("back\n")
+ssh_stdin.write("quit\n")
